3984ae497bb1288e80c4d73a7c39f34234a6713a
Cillian Barron <barroncillian@gmail.com>
2026-08-06 00:02:49 +0100

    fix(tesco): isolate soft-login refresh cookies (#145)

--- apps/api/src/features/tesco/auth/soft-login-auth-refresh.ts
+++ apps/api/src/features/tesco/auth/soft-login-auth-refresh.ts
@@ -1,5 +1,5 @@
 import { Effect, Layer, Option, Ref } from "effect";
-import { Cookies, Headers, HttpClient } from "effect/unstable/http";
+import { Headers, HttpClient } from "effect/unstable/http";
 
 import { AppConfig } from "../../../app/config.js";
 import { TescoAuthRefreshError } from "../tesco.errors.js";
@@ -22,90 +22,83 @@ export const TescoSoftLoginAuthRefreshLive = Layer.effect(
   Effect.gen(function* () {
     const config = yield* AppConfig;
     const client = yield* HttpClient.HttpClient;
-    const initialCookies = yield* cookiesFromHeader(
-      config.tesco.authCookieHeader
-    );
-    const cookieRef = yield* Ref.make(initialCookies);
-    const refreshClient = client.pipe(HttpClient.withCookiesRef(cookieRef));
-
-    const requestSoftLoginHtml = (
-      url: URL,
-      remainingRedirects: number
-    ): Effect.Effect<string, TescoAuthRefreshError> =>
-      Effect.gen(function* () {
-        const response = yield* refreshClient
-          .get(url, {
-            headers: {
-              accept:
-                "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-              "accept-language": config.tesco.locale,
-            },
-          })
-          .pipe(
-            Effect.mapError(
-              (cause) =>
-                new TescoAuthRefreshError(
-                  "Tesco soft login request failed",
-                  502,
-                  cause
-                )
-            )
-          );
-        yield* Ref.update(cookieRef, (cookies) =>
-          Cookies.merge(cookies, response.cookies)
-        );
-
-        if (response.status >= 300 && response.status < 400) {
-          if (remainingRedirects === 0) {
-            return yield* Effect.fail(
-              new TescoAuthRefreshError(
-                "Tesco soft login exceeded redirect limit",
-                502
-              )
-            );
-          }
-
-          const location = Headers.get(response.headers, "location");
-          if (Option.isNone(location)) {
-            return yield* Effect.fail(
-              new TescoAuthRefreshError(
-                "Tesco soft login redirect is missing Location",
-                502
-              )
-            );
-          }
-
-          return yield* requestSoftLoginHtml(
-            new URL(location.value, url),
-            remainingRedirects - 1
-          );
-        }
-
-        if (response.status < 200 || response.status >= 300) {
-          return yield* Effect.fail(
-            new TescoAuthRefreshError(
-              "Tesco soft login returned a non-success status",
-              response.status
-            )
-          );
-        }
-
-        return yield* response.text.pipe(
-          Effect.mapError(
-            (cause) =>
-              new TescoAuthRefreshError(
-                "Tesco soft login returned unreadable HTML",
-                502,
-                cause
-              )
-          )
-        );
-      });
 
     const refresh = (cookieHeader: TescoAuthCookieHeader) =>
       Effect.gen(function* () {
-        const cookies = yield* cookiesFromHeader(cookieHeader);
-        yield* Ref.set(cookieRef, cookies);
+        const initialCookies = yield* cookiesFromHeader(cookieHeader);
+        const cookieRef = yield* Ref.make(initialCookies);
+        const refreshClient = client.pipe(HttpClient.withCookiesRef(cookieRef));
+
+        const requestSoftLoginHtml = (
+          url: URL,
+          remainingRedirects: number
+        ): Effect.Effect<string, TescoAuthRefreshError> =>
+          Effect.gen(function* () {
+            const response = yield* refreshClient
+              .get(url, {
+                headers: {
+                  accept:
+                    "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+                  "accept-language": config.tesco.locale,
+                },
+              })
+              .pipe(
+                Effect.mapError(
+                  (cause) =>
+                    new TescoAuthRefreshError(
+                      "Tesco soft login request failed",
+                      502,
+                      cause
+                    )
+                )
+              );
+
+            if (response.status >= 300 && response.status < 400) {
+              if (remainingRedirects === 0) {
+                return yield* Effect.fail(
+                  new TescoAuthRefreshError(
+                    "Tesco soft login exceeded redirect limit",
+                    502
+                  )
+                );
+              }
+
+              const location = Headers.get(response.headers, "location");
+              if (Option.isNone(location)) {
+                return yield* Effect.fail(
+                  new TescoAuthRefreshError(
+                    "Tesco soft login redirect is missing Location",
+                    502
+                  )
+                );
+              }
+
+              return yield* requestSoftLoginHtml(
+                new URL(location.value, url),
+                remainingRedirects - 1
+              );
+            }
+
+            if (response.status < 200 || response.status >= 300) {
+              return yield* Effect.fail(
+                new TescoAuthRefreshError(
+                  "Tesco soft login returned a non-success status",
+                  response.status
+                )
+              );
+            }
+
+            return yield* response.text.pipe(
+              Effect.mapError(
+                (cause) =>
+                  new TescoAuthRefreshError(
+                    "Tesco soft login returned unreadable HTML",
+                    502,
+                    cause
+                  )
+              )
+            );
+          });
 
         const refreshUrl = new URL(config.tesco.softRefreshSignInUrl);
         refreshUrl.searchParams.set(
